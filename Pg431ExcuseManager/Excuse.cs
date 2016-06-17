80b5f41337c622a2412476b6eb8cc4c3a3a21828
gnivler <gnivil@gmail.com>
2016-06-17 15:07:03 -0600

    initial commit

---
+++ Pg431ExcuseManager/Excuse.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pg431ExcuseManager
+{
+    class Excuse
+    {
+        public string Description { get; set; }
+        public string Results { get; set; }
+        public DateTime LastUsed { get; set; }
+        public string ExcusePath { get; set; }
+
+
+        // overloaded triple constructors
+        // one for when the form is loaded
+        public Excuse()
+        {
+            
+        }
+
+        // one for opening a file
+        public Excuse(string fileToOpen)
+        {
+            ExcusePath = fileToOpen;    
+        }
+
+        // and one for a random excuse
+        public Excuse(int rand)
+        {
+
+        }
+
+
+        // run the save and open logic based on calls from the form, somehow?
+        public void OpenFile(string fileToOpen)
+        {
+
+        }
+
+        public void Save(string fileToSave)
+        {
+            
+        }
+    }
+
+}
--- Pg431ExcuseManager/Form1.cs
+++ Pg431ExcuseManager/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace Pg431ExcuseManager
 {
@@ -15,6 +16,85 @@ namespace Pg431ExcuseManager
         public Form1()
         {
             InitializeComponent();
+            currentExcuse.LastUsed = lastUsed.Value;
+        }
+
+        bool formChanged;
+        Excuse currentExcuse = new Excuse();
+        string selectedPath = "";
+        Random rand = new Random();
+
+        // trying new-fangled expression-bodied syntax for one line methods
+        private void description_TextChanged(object sender, EventArgs e)
+        {
+            currentExcuse.Description = description.Text;
+            UpdateForm(true);
+        }
+
+        private void results_TextChanged(object sender, EventArgs e)
+        {
+            currentExcuse.Results = results.Text;
+            UpdateForm(true);
+        }
+
+        private void lastUsed_ValueChanged(object sender, EventArgs e)
+        {
+            currentExcuse.LastUsed = lastUsed.Value;
+            UpdateForm(true);
+        }
+
+        // why does the provided code have 'this' ... does it not belong in the form?
+        private void UpdateForm(bool changed)
+        {
+            if (!changed)
+            {
+                this.description.Text = currentExcuse.Description;
+                this.results.Text = currentExcuse.Results;
+                this.lastUsed.Value = currentExcuse.LastUsed;
+                if (!string.IsNullOrEmpty(currentExcuse.ExcusePath))
+                {
+                    fileDate.Text = File.GetLastWriteTime(currentExcuse.ExcusePath).ToString();
+                    this.Text = "Execuse Manager";
+                }
+                else
+                {
+                    this.Text = "Execuse Manager*";
+                }
+                
+                // wtf is this?
+                this.formChanged = changed;
+            }
+        }
+
+        private void folder_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                selectedPath = folderBrowserDialog1.SelectedPath;
+                open.Enabled = true;
+                save.Enabled = true;
+                random.Enabled = true;
+            }
+        }
+
+        // call the Excuse object methods to open and save, somehow?
+        private void open_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.InitialDirectory = selectedPath;
+            Excuse excuse = new Excuse(selectedPath);
+
+        }
+
+        private void save_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.InitialDirectory = selectedPath;
+            // stuff
+            MessageBox.Show("Excuse file written");
+        }
+
+        private void random_Click(object sender, EventArgs e)
+        {
+            Excuse excuse = new Excuse(rand.Next());
         }
     }
 }
